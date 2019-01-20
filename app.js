79639073f4a5fa6b4c3fffe5c53f7883700f4665
w1lltj <cycorn8@gmail.com>
2019-01-20 21:52:44 +0800

    routing requests: render markup based on url

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+
+    const url = req.url;
+    
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<header><title>Enter Message</title></header>');
+        res.write(`<body>
+        <h1>Send Message to NodeJS Server..... </h1>
+        <form action="/message" method="POST">
+            <input type="text" name="message"/>
+            <button type="submit">Submit</button>
+        </form>
+        </body>`);
+        res.write('</html>');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title>First App</title></header>');
+    res.write('<body><h1>Hello from NodeJS Server!!!!!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
