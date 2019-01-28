8da1c1da2b100c76e4aafa61a19b1baa4e79420e
w1lltj <cycorn8@gmail.com>
2019-01-29 00:48:13 +0800

    Handle different routes: load different content depends on routes

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('inside middleware 1..... ');
-    next();
+/* 
+if you put the path '/' first, it will always load "Hello from ExpressJS". Because '/' is not fullpath, which
+means '/middleware' also contains '/'. Therefore, always put '/' at the last 
+*/
+
+app.use('/middleware', (req, res, next) => {
+    console.log('inside middleware 2..... ');
+    res.send('<h5>Hello from Middleware</h5>');
 });
 
-app.use((req, res, next) => {
-    console.log('inside middleware 2..... ');
-    res.send('<h5>Hello from ExpressJS</h5>');
+app.use('/', (req, res, next) => {
+    console.log('inside middleware 1..... ');
+    res.send('<h5>Hello from ExpressJS</h5>')
 });
 
 app.listen(3000);
