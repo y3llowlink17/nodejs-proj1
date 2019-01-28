774c0f7d397531710b82b2a635ecc86cdaede588
w1lltj <cycorn8@gmail.com>
2019-01-29 00:26:57 +0800

    using expressJS: next to move to the next middleware

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log('inside middleware 1..... ');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('inside middleware 2..... ');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
 
