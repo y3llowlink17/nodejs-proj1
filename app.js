56cab358bf234645f71977d9708e29bd52956562
w1lltj <cycorn8@gmail.com>
2019-02-03 12:00:39 +0800

    limit middleware execution to certain request

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ https://medium.com/@adamzerner/how-bodyparser-works-247897a93b90
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
+
+/*
+moving this line at the top now is OK. App works fine. Since we limit 
+the middleware execution with GET. Now, the path is fixed to "/". Therefore,
+It won't effect other line path as before
+*/
+app.get('/', (req, res, next) => {
+    res.send('<h5>Hello from ExpressJS</h5>');
+});
+
+
+app.get('/add-product', (req, res, next) => {
     console.log('inside add-product..... ');
     res.send(
         `<form action="/product" method="POST">
@@ -24,14 +35,18 @@ app.use('/add-product', (req, res, next) => {
         </form>`);
 });
 
-app.use('/product', (req, res, next) => {
+
+/*
+limit the middleware execution only to POST request (this code only executed by POST) & 
+ALSO, limit the path e.g: "/product" ONLY TO that path only.
+** when you use app.use, this DOES NOT limit the middleware execution. GET, POST, etc. can
+execute this.
+*/
+app.post('/product', (req, res, next) => {
     console.log('inside product..... ', req.body);
     res.redirect('/');
 });
 
-app.use('/', (req, res, next) => {
-    res.send('<h5>Hello from ExpressJS</h5>');
-});
 
 app.listen(3000);
 
