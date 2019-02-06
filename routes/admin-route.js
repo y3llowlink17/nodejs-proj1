2863abf0c18572e5f7762b13ce718fb896ff4685
w1lltj <cycorn8@gmail.com>
2019-02-06 23:10:24 +0800

    serve HTML page template

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -1,24 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();    //express.Router() is kind of mini express. Works similar to express.
 
 router.get('/add-product', (req, res, next) => {
     console.log('inside add-product..... ');
 
-    /* 
-    can change the action="./add-product" or action="/admin/add-product". 
-    Both work the same. But, I find "./add-product" (relative path way) 
-    is more scallable since it is not hard-coded
-     */
-    res.send(
-        `<form action="./add-product" method="POST">
-            <input type="text" name="product-name"></input>
-            <button type="submit">Submit</button>
-        </form>`);
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-// using similar path only the request method is different.
-// this works the same
+
 router.post('/add-product', (req, res, next) => {
     console.log('inside product..... ', req.body);
     res.redirect('/');
--- routes/shop-route.js
+++ routes/shop-route.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h5>Hello from ExpressJS</h5>');
+    // path must be absolute or specify root to res.sendFile
+    // "__dirname" points to absolute path of "routes"
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
