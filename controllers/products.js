0919909bfcd5da87667fbfd316a28106d13e6484
w1lltj <cycorn8@gmail.com>
2019-03-21 17:20:35 +0800

    add product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+const Product = require('../models/product');
+
 
 exports.getAddProduct = (req, res, next) => {
     console.log('inside add-product..... ');
@@ -9,11 +10,15 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
     console.log('inside product..... ', req.body);
 
-    products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
+    
     res.redirect('/');
 };
 
 exports.getProduct = (req, res, next) => {
+    const products = Product.fetchAll()
+
     res.render('shop', {items: products, docTitle: 'Shop', path: '/'});
 
     console.log('shop-route_products..... ', products);
